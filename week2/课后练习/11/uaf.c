11f2f99ac39416ffd06005e98e587c3d54eeccdb
Youmoguolicheng <zangwenshang@qq.com>
2026-09-04 22:51:43 +0800

    完成课后第十一题

---
+++ week2/课后练习/11/uaf.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int main() {
+    char *greeting = malloc(32);
+    strcpy(greeting, "Hello, world!");
+    printf("%s\n", greeting);
+
+    free(greeting);
+
+    return 0;
+}
